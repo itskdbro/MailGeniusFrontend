c101f5e4d827408dad109cbfa8426a233017d27b
itskdbro <keshavdhawan2002@gmail.com>
2025-02-08 18:21:10 +0530

    Layout fixing

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,15 @@ import {
   MailSearch,
   MessageCircleMore,
 } from "lucide-react";
-
 function App() {
   return (
     <div
-      className="w-full h-screen px-6  flex justify-center items-center "
+      className="w-full min-h-screen px-6 flex justify-center items-center"
       style={{
         backgroundImage: `url(${bg2})`,
       }}
     >
-      <Card className="w-full h-[35em] max-w-4xl mx-auto bg-zinc-950 outline-none text-white  rounded-2xl shadow-lg border border-white border-opacity-20 bg-black/50 backdrop-blur-lg ">
+      <Card className="relative w-full max-h-[90vh] max-w-4xl mx-auto bg-zinc-950 outline-none text-white rounded-2xl shadow-lg border border-white border-opacity-20 bg-black/50 backdrop-blur-lg overflow-hidden">
         <CardHeader>
           <div className="flex flex-col items-center justify-center text-center">
             <div className="flex items-center gap-2">
@@ -30,12 +29,12 @@ function App() {
               </CardTitle>
               <MessageCircleMore size={30} />
             </div>
-            <p className="text-[15px]  text-zinc-500">
+            <p className="text-[15px] text-zinc-500">
               Genius at replying to emails
             </p>
           </div>
         </CardHeader>
-        <CardContent className="sticky top-[35rem]">
+        <CardContent className="relative">
           <EmailReplyForm />
         </CardContent>
       </Card>
